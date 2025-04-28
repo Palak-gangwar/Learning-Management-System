e41c7c5faf3e4d99e9facdd17683847d5ee0c7fe
Palak-gangwar <palakgangwar01@gmail.com>
2025-04-28 12:18:53 +0530

    Add files via upload

---
+++ Client/src/features/api/courseApi.js
@@ -0,0 +1,142 @@
+import EditCourse from "@/pages/admin/course/EditCourse";
+import CreateLecture from "@/pages/admin/lecture/createLecture";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const COURSE_API = "http://localhost:8080/api/v1/course";
+
+export const courseApi = createApi({
+  reducerPath: "courseApi",
+  tagTypes: ["refetch", "refetchLecture"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: COURSE_API,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    createCourse: builder.mutation({
+      query: ({ courseTitle, category }) => ({
+        url: "",
+        method: "POST",
+        body: { courseTitle, category },
+        // headers: { "Content-Type": "application/json" },
+      }),
+      invalidatesTags: ["refetch"],
+    }),
+    getSearchCourse: builder.query({
+      query: ({ searchQuery, categories, sortByPrice }) => {
+        // Build qiery string
+        let queryString = `/search?query=${encodeURIComponent(searchQuery)}`;
+
+        // append cateogry
+        if (categories && categories.length > 0) {
+          const categoriesString = categories.map(encodeURIComponent).join(",");
+          queryString += `&categories=${categoriesString}`;
+        }
+
+        // Append sortByPrice is available
+        if (sortByPrice) {
+          queryString += `&sortByPrice=${encodeURIComponent(sortByPrice)}`;
+        }
+
+        return {
+          url: queryString,
+          method: "GET",
+        };
+      },
+    }),
+    getPublishedCourse: builder.query({
+      query: () => ({
+        url: "/published-courses",
+        method: "GET",
+      }),
+    }),
+    getCreatorCourse: builder.query({
+      query: () => ({
+        url: "",
+        method: "GET",
+        // body: {courseTitle, category}
+      }),
+      invalidatesTags: ["refetch"],
+    }),
+    editCourse: builder.mutation({
+      query: ({ formdata, courseId }) => ({
+        url: `/${courseId}`,
+        method: "PUT",
+        body: formdata,
+      }),
+      invalidatesTags: ["refetch"],
+    }),
+    getCourseById: builder.query({
+      query: (courseId) => ({
+        url: `/${courseId}`,
+        method: "GET",
+      }),
+      invalidatesTags: ["refetch"],
+    }),
+    createLecture: builder.mutation({
+      query: ({ lectureTitle, courseId }) => ({
+        url: `/${courseId}/lecture`,
+        method: "POST",
+        body: { lectureTitle },
+      }),
+    }),
+    getCourseLecture: builder.query({
+      query: (courseId) => ({
+        url: `/${courseId}/lecture`,
+        method: "GET",
+      }),
+      providesTags: ["refetchLecture"],
+    }),
+    editLecture: builder.mutation({
+      query: ({
+        lectureTitle,
+        videoInfo,
+        isPreviewFree,
+        courseId,
+        lectureId,
+      }) => ({
+        url: `/${courseId}/lecture/${lectureId}`,
+        method: "POST",
+        body: {
+          lectureTitle,
+          videoInfo,
+          isPreviewFree,
+        },
+      }),
+    }),
+    removeLecture: builder.mutation({
+      query: (lectureId) => ({
+        url: `/lecture/${lectureId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["refetchLecture"],
+    }),
+
+    getLectureById: builder.query({
+      query: (lectureId) => ({
+        url: `/lecture/${lectureId}`,
+        method: "GET",
+      }),
+    }),
+    publishCourse: builder.mutation({
+      query: ({ courseId, query }) => ({
+        url: `/${courseId}?publish=${query}`,
+        method: "PATCH",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateCourseMutation,
+  useGetSearchCourseQuery,
+  useGetPublishedCourseQuery,
+  useGetCreatorCourseQuery,
+  useEditCourseMutation,
+  useGetCourseByIdQuery,
+  useCreateLectureMutation,
+  useGetCourseLectureQuery,
+  useEditLectureMutation,
+  useRemoveLectureMutation,
+  useGetLectureByIdQuery,
+  usePublishCourseMutation,
+} = courseApi;
